9cfe888e63a3bc97270c336b1eece2087017c8c9
Shunsuke Yamazaki <sa@shunsuke-no-MacBook-Air.local>
2016-07-16 23:36:13 +0900

    :recycle: Use NSNotificationCenter.

--- watchdogs3/StatusMenuController.swift
+++ watchdogs3/StatusMenuController.swift
@@ -12,7 +12,6 @@ class StatusMenuController: NSObject, NSUserNotificationCenterDelegate {
 
     @IBOutlet weak var statusMenu: NSMenu!
     
-    var timer: NSTimer!
     let format = "%02d:%02d"
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
     
@@ -28,15 +27,40 @@ class StatusMenuController: NSObject, NSUserNotificationCenterDelegate {
     
     override init() {
         super.init()
-        
-        PomodoroTimer.sharedInstance.addObserver(self, forKeyPath: "myDate", options: .New, context: &myDateContext)
-        // TODO: 本当はこんな事しなくてもdelegateあたりを使えば良いっぽい
-        PomodoroTimer.sharedInstance.addObserver(self, forKeyPath: "workTime", options: .New, context: &workTimeContext)
-        PomodoroTimer.sharedInstance.addObserver(self, forKeyPath: "restTime", options: .New, context: &restTimeContext)
-        
+        PomodoroTimer.sharedInstance.addNotificationCenterObserver(observer: self,
+                                                                   selector: #selector(countDown(_:)),
+                                                                   name: PomodoroTimer.COUNT_DOWN,
+                                                                    userInfo: nil)
+        PomodoroTimer.sharedInstance.addNotificationCenterObserver(observer: self,
+                                                                   selector: #selector(updatePrefTimes(_:)),
+                                                                   name: PomodoroTimer.UPDATE_TIME_SETTING,
+                                                                    userInfo: nil)
         userData = NSUserDefaults.standardUserDefaults()
     }
 
+    func countDown(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            var time = userInfo["time"] as! Double
+            
+            if (time <= 0.0) {
+                statusItem.title = String(format: format, 0, 0)
+                notificationForTimeUp(PomodoroTimer.sharedInstance.status)
+                startTimer("")
+                return
+            }
+            time += 0.1 // 59秒対策
+            // 0.9秒で呼ばれる時があるので、23.000の時に0.01で調整する
+            statusItem.title = String(format: format, Int(time / 60.0), Int(time % 60.0 - 0.01))
+        }
+    }
+    
+    func updatePrefTimes(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let work = userInfo["work"] as! Int
+            statusItem.title = String(format: format, work ?? 25, 0)
+        }
+    }
+    
     override func awakeFromNib() {
         workTime = userData.integerForKey("workTime")
         breakTime = userData.integerForKey("breakTime")
@@ -51,53 +75,16 @@ class StatusMenuController: NSObject, NSUserNotificationCenterDelegate {
         
         preferencesWindow = PreferencesWindow()
     }
-    
-    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
-        print("observeValueForKeyPath   \(keyPath)")
         
-        if context == &myDateContext {
-            let t = PomodoroTimer.sharedInstance.myDate
-            
-            if (t <= 0.0) {
-                statusItem.title = String(format: format, 0, 0)
-                timer.invalidate()
-                notificationForTimeUp(PomodoroTimer.sharedInstance.status)
-                startTimer("")
-                return
-            }
-            
-            // 0.9秒で呼ばれる時があるので、23.000の時に0.01で調整する
-            statusItem.title = String(format: format, Int(t / 60.0), Int(t % 60.0 - 0.01))
-        } else if context == &workTimeContext || context == &restTimeContext {
-            if let _ = timer {
-                timer.invalidate()
-            }
-            
-            let t = PomodoroTimer.sharedInstance.workTime
-            statusItem.title = String(format: format, Int(t) ?? 25, 0)
-        } else {
-            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
-        }
-    }
-    
     @IBAction func startTimer(sender: AnyObject) {
-        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(countdown(_:)), userInfo: nil, repeats: true)
-        NSRunLoop.currentRunLoop().addTimer(timer!, forMode: NSRunLoopCommonModes)
-        
         PomodoroTimer.sharedInstance.start()
     }
 
     @IBAction func suspendTimer(sender: NSMenuItem) {
-        if let _ = timer {
-            timer.invalidate()
-        }
+        PomodoroTimer.sharedInstance.suspend()
     }
     
     @IBAction func resetTimer(sender: NSMenuItem) {
-        if let _ = timer {
-            timer.invalidate()
-        }
-        
         PomodoroTimer.sharedInstance.reset()
         statusItem.title = String(format: format, workTime, 0)
     }
@@ -108,36 +95,22 @@ class StatusMenuController: NSObject, NSUserNotificationCenterDelegate {
     }
     
     @IBAction func showPreference(sender: AnyObject) {
-        if let _ = timer {
-            timer.invalidate()
-        }
-
+        PomodoroTimer.sharedInstance.reset()
         preferencesWindow.showWindow(nil)
     }
     
-    func countdown(timer: NSTimer) {
-        PomodoroTimer.sharedInstance.updateDate(NSDate())
-    }
-    
     func notificationForTimeUp(status: PomodoroTimer.Status) {
+        print("notificationForTimeUp : \(status)")
         let notification = NSUserNotification()
 
         notification.deliveryDate = NSDate()
 
         if (status == PomodoroTimer.Status.Working) {
-            notification.title = "働きましょう"
-        } else {
             notification.title = "休憩しましょう"
+        } else {
+            notification.title = "働きましょう"
         }
 
         NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
-        print("notificationForTimeUp: \(NSUserNotificationCenter.defaultUserNotificationCenter().deliveredNotifications)")
     }
-    
-    deinit {
-        PomodoroTimer.sharedInstance.removeObserver(self, forKeyPath: "myDate", context: &myDateContext)
-        PomodoroTimer.sharedInstance.removeObserver(self, forKeyPath: "workTime", context: &workTimeContext)
-        PomodoroTimer.sharedInstance.removeObserver(self, forKeyPath: "restTime", context: &restTimeContext)
-    }
-
 }
