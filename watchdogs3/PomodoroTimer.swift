63a3b8cdbcac3c8153b7040af96fc9d4d471cc3e
Shunsuke Yamazaki <sa@shunsuke-no-MacBook-Air.local>
2016-07-21 00:07:32 +0900

    :recycle:

--- watchdogs3/PomodoroTimer.swift
+++ watchdogs3/PomodoroTimer.swift
@@ -22,11 +22,14 @@ class PomodoroTimer: NSObject {
     var endTime : NSDate! = nil             // 終了時間を表す変数
     var userData: NSUserDefaults!
     var observerObject: NSObject!
-
+    
     weak var timer: NSTimer?
 
     static let COUNT_DOWN = "COUNT_DOWN"
     static let UPDATE_TIME_SETTING = "UPDATE_TIME_SETTING"
+    
+    static let KEY_WORK_TIME = "workTime"
+    static let KEY_REST_TIME = "restTime"
     
     class var sharedInstance: PomodoroTimer {
         struct Singleton {
@@ -53,7 +56,7 @@ class PomodoroTimer: NSObject {
         }
 
         print("start status: \(status)")
-
+        
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ticktock), userInfo: nil, repeats: true)
         NSRunLoop.currentRunLoop().addTimer(timer!, forMode: NSRunLoopCommonModes)
 
@@ -67,7 +70,8 @@ class PomodoroTimer: NSObject {
             return
         }
 
-        let time_left: Double = endTime.timeIntervalSinceDate(now)
+        // 0.9秒で呼ばれる時があるので、23.000の時に0.1で調整する
+        let time_left: Double = endTime.timeIntervalSinceDate(now) - 0.1
         print("ticktock \(time_left)" )
         NSNotificationCenter.defaultCenter().postNotificationName(PomodoroTimer.COUNT_DOWN, object: observerObject, userInfo: ["time" : time_left])
     }
@@ -78,7 +82,7 @@ class PomodoroTimer: NSObject {
         }
 
         suspend()
-        currentSettingTime("workTime")
+        currentSettingTime(PomodoroTimer.KEY_WORK_TIME)
         status = Status.Working
     }
 
@@ -88,14 +92,14 @@ class PomodoroTimer: NSObject {
         }
 
         suspend()
-        currentSettingTime("restTime")
+        currentSettingTime(PomodoroTimer.KEY_REST_TIME)
         status = Status.Resting
     }
     
     func reset() {
         suspend()
         status = Status.Stopped
-        print("status: \(status)")
+//        print("status: \(status)")
     }
     
     func suspend() {
@@ -107,11 +111,11 @@ class PomodoroTimer: NSObject {
             userData = NSUserDefaults.standardUserDefaults()
         }
 
-//        let start = userData.integerForKey(key)
-//        let interval = Double(start) * 60.0
-        let interval = 5.0
+        let start = userData.integerForKey(key)
+        let interval = Double(start) * 61.0
+//        let interval = 5.0    デバック
         endTime = NSDate(timeInterval: interval, sinceDate: NSDate())
-        print("currentSettingTime \(endTime)")
+        print("currentSettingTime \(endTime), \(start), \(interval), \(userData)")
     }
 
     func addNotificationCenterObserver(observer target: NSObject, selector aSelector: Selector, name: String, userInfo anObject: AnyObject?) {
